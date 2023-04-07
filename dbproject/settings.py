dbc396c7321b34476eefeff264f29bfa1f2ae40e
matthewhelan <36001451+matthewhelan@users.noreply.github.com>
2023-04-06 20:00:55 -0400

    init + index.html

--- dbproject/settings.py
+++ dbproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'parlaypro'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = "dbproject.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = "dbproject.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        'NAME' : 'ssb3vk',
+        'HOST':"mysql01.cs.virginia.edu", 
+        'USER':"ssb3vk",
+        'PASSWORD':"WrFeK77hYZb3fxN",
+        'PORT' : '3306'
     }
 }
 
